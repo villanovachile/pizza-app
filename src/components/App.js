658779b219318060cb42f6eb5942a6af1633c31f
Synora <synora.eusebio@automattic.com>
2023-03-03 19:52:24 -0500

    new components and CSS (#4)
    Co-authored-by: Synora Eusebio <synora.eusebio@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import PizzaSize from './Pizza-Size.js';
 import Crust from './Crust.js';
 import Toppings from './Toppings.js';
+import Selection from './Selection.js';
+import SelectedItems from './SelectedItems.js';
+import Order from './Order.js';
 
 {/*Testing*/}
 
@@ -11,9 +14,23 @@ function App() {
       <header className="App-header">
         <h1>Pizza App</h1>
       </header>
-      <PizzaSize />
-      <Crust />
-      <Toppings />
+    <div className="container">
+      <div className="column">
+        <Selection>
+        {/*Renders order options on the left side of the page*/}
+        </Selection>
+      </div>
+      <div className="column">
+        <SelectedItems>
+        {/*Renders selected items on the right side of the page*/}
+        </SelectedItems>
+      </div>
+    </div>
+      <div className="order">
+        <Order>
+        {/*Order component for the bottom of the page*/}
+        </Order>
+      </div>
     </div>
   );
 }
---
+++ src/components/SelectedItems.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PizzaSize from './Pizza-Size.js';
+import Crust from './Crust.js';
+import Toppings from './Toppings.js';
+import index from '../index.css';
+
+
+function SelectedItems() {
+    return(
+        <div className="selectedItems">
+           <h1>Selected Items</h1> 
+        </div>
+    )
+}
+
+export default SelectedItems;
