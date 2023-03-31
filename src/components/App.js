7347be0d29519bf5a4e004d2fe258c85d27f2375
Daniel R <daniel.r@automattic.com>
2023-03-31 16:14:52 -0700

    Updated component structure and styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,19 @@
-import React from 'react';
-import PizzaSize from './Pizza-Size.js';
-import Crust from './Crust.js';
-import Toppings from './Toppings.js';
-import Selection from './Selection.js';
-import SelectedItems from './SelectedItems.js';
-import Order from './Order.js';
-
-{/*Testing*/}
-
-
+import React from "react";
+import PizzaSize from "./PizzaSize.js";
+import Toppings from "./Toppings.js";
+import CrustOptions from "./CrustOptions.js";
+import OrderSummary from "./OrderSummary.js";
 
 function App(props) {
   return (
-    <div className="App">
+    <div className="pizza-app-container">
       <header className="App-header">
         <h1>Pizza App</h1>
       </header>
-    <div className="container">
-      <div className="column">
-        <Selection data={props.data}>
-        {/*Renders order options on the left side of the page*/}
-        </Selection>
-      </div>
-      <div className="column">
-        <SelectedItems>
-        {/*Renders selected items on the right side of the page*/}
-        </SelectedItems>
-      </div>
-    </div>
-      <div className="order">
-        <Order>
-        {/*Order component for the bottom of the page*/}
-        </Order>
-      </div>
+      <PizzaSize />
+      <CrustOptions />
+      <Toppings />
+      <OrderSummary />
     </div>
   );
 }
--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -1,40 +1,43 @@
-import React from 'react';
-import Selection from './Selection.js';
+import { updateTopping } from "../options/optionsSlice";
+import { useSelector, useDispatch } from "react-redux";
+import ToppingItem from "./ToppingItem";
 
-function Toppings() {
-    return(
-        <>
-            <p> 
-                Choose your Toppings: 
-                <br />
-                <label>
-                    <input type="radio" name="toppingsChoice" value="sauce" />         
-                    Sauce
-                </label> 
-                <br />
-                <label>
-                    <input type="radio" name="toppingsChoice" value="pepperoni" />
-                    Pepperoni
-                </label> 
-                <br />
-                <label>
-                    <input type="radio" name="toppingsChoice" value="onion" />
-                    Onion
-                </label>
-                <br />
-                <label>
-                    <input type="radio" name="toppingsChoice" value="sausage" />
-                    Sausage
-                </label>
-                <br />
-                <label>
-                    <input type="radio" name="toppingsChoice" value="pineapple" />
-                    Pineapple
-                </label>
-         </p>
+const Toppings = () => {
+  const dispatch = useDispatch();
 
-        </>
-    );
-}
+  const toppings = useSelector((state) => state.options.toppings);
+
+  const updateToppingSelection = (toppingName, selection) => {
+    dispatch(updateTopping({ name: toppingName, selection: selection }));
+  };
+
+  //   updateToppingSelection("pepperoni", "right");
+
+  const getToppingSelection = (toppingName) => {
+    const topping = toppings.find((t) => t.name === toppingName);
+    if (topping) {
+      return topping.selection;
+    } else {
+      return null;
+    }
+  };
+
+  //   console.log("Pepperoni selection:", getToppingSelection("pepperoni"));
+
+  return (
+    <div className="toppings-container">
+      {toppings.map((topping) => (
+        <div className="topping-items" key={topping.name}>
+          <ToppingItem
+            topping={topping.name}
+            selection={topping.selection}
+            getToppingSelection={getToppingSelection}
+            updateToppingSelection={updateToppingSelection}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Toppings;
