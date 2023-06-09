cc0f411ff014a410f0d25422855a4b1d6c4bedbe
Synora <synora.eusebio@automattic.com>
2023-06-09 11:03:24 -0400

    Rendering Pizza Size and Crust Selections (#23)
    * hooking PizzaSize back up to state
    
    * render size and crust options on browser
    
    ---------
    
    Co-authored-by: Synora Eusebio <synora.eusebio@gmail.com>

--- src/components/OrderSummary.js
+++ src/components/OrderSummary.js
@@ -1,5 +1,30 @@
+import { useSelector, useDispatch } from "react-redux";
+
 const OrderSummary = () => {
-  return <div className="order-summary">Order Total: $20</div>;
+
+  const size = useSelector((state) => state.options.size);
+  const sizeSelection = useSelector((state) => state.orders[0].pizza.size);
+  const renderSizeSelection = size.map((item) => {
+   if (item.id === sizeSelection) {
+    return item.name;
+    };
+  });
+
+  const crusts = useSelector((state) => state.options.crusts);
+  const crustSelection = useSelector((state) => state.orders[0].pizza.crust); 
+  const renderCrustSelection = crusts.map((item) => {
+    if (item.id === crustSelection) {
+     return item.name;
+     };
+   });
+
+  return (
+  <div className="Summary">
+    <div className="sizeSelction">Size: {renderSizeSelection}</div>
+    <div className="crustSelection"> Crust: {renderCrustSelection}</div>
+    <div className="order-summary">Order Total: $20</div>
+  </div>
+  );
 };
 
 export default OrderSummary;
